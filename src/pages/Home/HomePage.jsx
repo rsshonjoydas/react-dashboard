997122e3181bb3f0f91c3283079235ca73de5a80
rsshonjoydas <rsshonjoydas@gmail.com>
2021-06-17 05:01:30 +0600

    chart use recharts

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,12 @@
+import Chart from '../../components/Chart/Chart';
+import ChartData from '../../components/Chart/ChartData';
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
 import styled from './HomePage.module.css';
 
 const HomePage = () => (
   <div className={styled.home}>
     <FeaturedInfo />
+    <Chart data={ChartData} title="User Analytics" grid dataKey="Active User" />
   </div>
 );
 
