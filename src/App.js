add771fc011af591c03053e96a95dad769d055f5
rsshonjoydas <rsshonjoydas@gmail.com>
2021-06-18 11:39:54 +0600

    product

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Sidebar from './components/Sidebar/Sidebar';
 import TopBar from './components/TopBar/TopBar';
 import HomePage from './pages/Home/HomePage';
 import NewUser from './pages/newUser/newUser';
+import Product from './pages/product/Product';
+import ProductList from './pages/productList/ProductList';
 import User from './pages/user/User';
 import UserList from './pages/userList/UserList';
 
@@ -25,6 +27,12 @@ function App() {
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
           </Route>
         </Switch>
       </div>
---
+++ src/pages/product/ProductData.js
@@ -0,0 +1,16 @@
+const productData = [
+  {
+    name: 'Jan',
+    Sales: 4000,
+  },
+  {
+    name: 'Feb',
+    Sales: 3000,
+  },
+  {
+    name: 'Mar',
+    Sales: 5000,
+  },
+];
+
+export default productData;
